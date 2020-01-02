456354580908576970686b570aee169b29b1b3a2
Azamat Mukhiddinov <25627421+mazamats@users.noreply.github.com>
2020-01-02 09:02:17 -0700

    jank globals usage

--- main.go
+++ main.go
@@ -2,25 +2,49 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"strconv"
 	"time"
+
+	"github.com/blockcypher/gobcy"
+	"github.com/labstack/echo"
 )
 
-var blockHeight int = 0
+var heights = map[string]int{
+	"eth":  0,
+	"btc":  0,
+	"ltc":  0,
+	"doge": 0,
+	"dash": 0,
+}
 
-func checkExpire() {
+func updateHeight(chain string) {
+	bc := gobcy.API{"", chain, "main"}
 	for {
-		blockHeight++
-		fmt.Printf("Set height to: " + strconv.Itoa(blockHeight))
-		time.Sleep(1000 * time.Millisecond)
+		data, err := bc.GetChain()
+		if err != nil {
+			log.Fatal(err)
+		}
+		heights[chain] = data.Height
+		fmt.Println("Set " + chain + " height to: " + strconv.Itoa(data.Height))
+		time.Sleep(60 * time.Second)
 	}
 }
 
+func handler(c echo.Context) error {
+	chain := c.Param("chain")
+	if _, ok := heights[chain]; ok {
+		return c.String(http.StatusOK, strconv.Itoa(heights[chain]))
+	}
+	return c.String(http.StatusNotFound, "Invalid chain")
+}
+
 func main() {
-	go checkExpire()
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "ETH Blockheight: %s!", strconv.Itoa(blockHeight))
-	})
-	http.ListenAndServe(":8080", nil)
+	for k := range heights {
+		go updateHeight(k)
+	}
+	e := echo.New()
+	e.GET("/:chain", handler)
+	e.Logger.Fatal(e.Start(":8080"))
 }
